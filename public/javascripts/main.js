a47105e3ca1ea2acf5a86b6e50ec722ea0fc10db
damilog <dami.k.dev@gmail.com>
2021-03-09 16:17:04 +0900

    feat : git commit test

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -2,7 +2,11 @@ import TopCarouselUI from "./TopCarouselUI.js";
 import BottomCarouselUI from "./BottomCarouselUI.js";
 import ViewMoreUI from "./ViewMoreUI.js";
 import _ from "./util.js";
+import SearchBarUI from "./SearchBarUI.js";
 
 const topCarouselUI = new TopCarouselUI();
 const viewMoreUI = new ViewMoreUI();
 const bottomCarouselUI = new BottomCarouselUI();
+const searchBarUI = new SearchBarUI();
+
+searchBarUI.get();
