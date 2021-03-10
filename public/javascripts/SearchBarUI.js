72ab9d517fd8478cb4103f4edbb65183c4a24922
damilog <dami.k.dev@gmail.com>
2021-03-10 18:10:04 +0900

    feat : 검색창 롤링 기능 UI, render 기능 추가

--- public/javascripts/SearchBarUI.js
+++ public/javascripts/SearchBarUI.js
@@ -1,9 +1,45 @@
-function SearchBarUI() {
-  this.get = async function () {
-    const res = await fetch(
-      "https://m.shoppinghow.kakao.com/v3/m/search/extends.json?_=1615270019198"
-    );
-    this.result = await res.json();
-    console.log(this.result);
-  };
-}
+import _ from "./util";
+
+const SearchBarUI = function (url, listWrap) {
+  this.url = url;
+  this.$rollingSearchList = listWrap;
+  this.rollSize = 0;
+  console.log(33);
+};
+
+SearchBarUI.prototype.getJson = async function () {
+  const res = await fetch(this.url);
+  this.result = await res.json();
+  return this.result.upkeyword;
+};
+
+SearchBarUI.prototype.drawKeywordList = async function () {
+  const keywordList = await this.getJson();
+  const keywordListTemplate = keywordList.reduce((acc, list, idx) => {
+    const template = `<li><span class="list-rank">${
+      idx + 1
+    }</span>${list}</li>`;
+    return acc + template;
+  }, "");
+  this.$rollingSearchList.insertAdjacentHTML("afterbegin", keywordListTemplate);
+
+  const keywordFirstChild = this.$rollingSearchList.firstElementChild.cloneNode(
+    true
+  );
+  this.$rollingSearchList.appendChild(keywordFirstChild);
+};
+
+SearchBarUI.prototype.setToRepeatRoll = function () {
+  const timer = setInterval(this.roll.bind(this), 1000);
+  if (this.rollSize === 10) clearInterval(timer);
+};
+
+SearchBarUI.prototype.roll = function () {
+  this.$rollingSearchList.style.transition = "transform 0.3s ease-in-out";
+
+  this.$rollingSearchList.style.transform =
+    "translateY(" + -32 * this.rollSize + "px)";
+  this.rollSize++;
+};
+
+export { SearchBarUI };
--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -1,12 +1,21 @@
+import "regenerator-runtime/runtime";
 import TopCarouselUI from "./TopCarouselUI.js";
 import BottomCarouselUI from "./BottomCarouselUI.js";
 import ViewMoreUI from "./ViewMoreUI.js";
 import _ from "./util.js";
-import SearchBarUI from "./SearchBarUI.js";
+import { SearchBarUI } from "./SearchBarUI.js";
+
+const $rollingSearchList = _.$(".roll-search-list");
 
 const topCarouselUI = new TopCarouselUI();
 const viewMoreUI = new ViewMoreUI();
 const bottomCarouselUI = new BottomCarouselUI();
-const searchBarUI = new SearchBarUI();
+const searchBarUI = new SearchBarUI(
+  "https://m.shoppinghow.kakao.com/v3/m/search/extends.json",
+  $rollingSearchList
+);
 
-searchBarUI.get();
+searchBarUI.getJson();
+searchBarUI.drawKeywordList();
+searchBarUI.setToRepeatRoll();
+searchBarUI.roll();
