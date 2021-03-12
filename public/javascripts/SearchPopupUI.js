19fc17c65d42d5a021c7a0b7fefad303f21a0f5b
damilog <dami.k.dev@gmail.com>
2021-03-13 00:52:26 +0900

    feat : 연관 검색어 리스트 팝업 기능 추가

--- public/javascripts/SearchPopupUI.js
+++ public/javascripts/SearchPopupUI.js
@@ -4,20 +4,29 @@ import _ from "./util";
 const SearchPopupUI = function (url, input) {
   this.url = url;
   this.$input = input;
+  this.activeTimer;
+  this.currentRelatedKeywordList = [];
   this.init();
 };
 
 SearchPopupUI.prototype = {
   constructor: SearchPopupUI,
-  async getJson() {
+
+  init() {
+    this.$input.addEventListener("focus", this.showPopUpBox);
+    this.$input.addEventListener("input", this.showRelatedKeyword.bind(this));
+    window["getResponseJsonpData"] = this.getResponseJsonpData.bind(this);
+  },
+
+  async requestJson() {
     //중복 코드 제거 필요
     const res = await fetch(this.url);
     this.result = await res.json();
     return this.result.upkeyword;
   },
 
-  async renderPopUpBox() {
-    const keywordList = await this.getJson();
+  async renderPopupBox() {
+    const keywordList = await this.requestJson();
     const innerTemplate = keywordList.reduce((acc, list, idx) => {
       let template;
       switch (idx) {
@@ -46,11 +55,6 @@ SearchPopupUI.prototype = {
     _.$(".popup-search-wrap").insertAdjacentHTML("afterbegin", outerTemplate);
   },
 
-  init() {
-    this.$input.addEventListener("focus", this.showPopUpBox);
-    this.$input.addEventListener("input", this.hidePopUpBox);
-  },
-
   showPopUpBox() {
     _.$(".popup-search-wrap").style.display = "block";
     _.$(".roll-search-wrap").style.display = "none";
@@ -58,6 +62,50 @@ SearchPopupUI.prototype = {
 
   hidePopUpBox() {
     _.$(".popup-search-wrap").style.display = "none";
+  },
+
+  requestJsonp(word, callbackName) {
+    const script = document.createElement("script");
+    script.src = `https://suggest-bar.daum.net/suggest?callback=${callbackName}&limit=10&mode=json&code=utf_in_out&q=${word}&id=shoppinghow_suggest`;
+    document.body.append(script);
+  },
+
+  getResponseJsonpData(data) {
+    this.currentRelatedKeywordList = data.items.map(x =>
+      x.replace(/\|.+/g, "")
+    );
+  },
+
+  drawRelatedKeywordBox() {
+    const $popupWrap = _.$(".popup-search-wrap__inner");
+    const innerTemplate = this.currentRelatedKeywordList.reduce((acc, cur) => {
+      return acc + ` <li>${cur}</li>`;
+    }, "");
+
+    const outerTemplate = `<ol class="inner__keyword-list">${innerTemplate}</ol>`;
+    $popupWrap.innerHTML = outerTemplate;
+    _.$(".inner__keyword-list").style.display = "block";
+  },
+
+  getInputData() {
+    const currentKeyword = _.$(".search-form__input").value;
+    this.requestJsonp(currentKeyword, "getResponseJsonpData");
+  },
+
+  async showRelatedKeyword() {
+    await this.getInputValue();
+    this.drawRelatedKeywordBox();
+  },
+
+  getInputValue() {
+    if (!this.activeTimer) {
+      this.activeTimer = _.debounce(this.getInputData.bind(this), 500);
+    }
+    this.activeTimer();
+  },
+
+  colorKeyword() {
+    //검색어와 일치하는 list 문자에 coloring
   },
 };
 
--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -5,8 +5,6 @@ import ViewMoreUI from "./ViewMoreUI.js";
 import _ from "./util.js";
 import { SearchBarUI } from "./SearchBarUI.js";
 import { SearchPopupUI } from "./SearchPopupUI.js";
-import { RelatedSearchUI } from "./RelatedSearchUI.js";
-// import { RelatedSearchUI  } from "./RelatedSearchUI .js";
 
 const API = "https://shoppinghow.kakao.com/v3/m/search/extends.json";
 const $input = _.$(".search-form__input");
@@ -14,13 +12,10 @@ const $input = _.$(".search-form__input");
 const topCarouselUI = new TopCarouselUI();
 const viewMoreUI = new ViewMoreUI();
 const bottomCarouselUI = new BottomCarouselUI();
+
 const searchBarUI = new SearchBarUI(API);
 const searchPopupUI = new SearchPopupUI(API, $input);
-
 searchBarUI.getJson();
 searchBarUI.renderKeywordList();
 searchBarUI.setToRepeatRoll();
-searchPopupUI.renderPopUpBox();
-
-const relatedSearchUI = new RelatedSearchUI($input);
-relatedSearchUI.requestJsonp("고구마", "responseJsonpData");
+searchPopupUI.renderPopupBox();
